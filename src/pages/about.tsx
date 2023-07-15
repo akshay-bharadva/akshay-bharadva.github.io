35b818dc095afeedfeb0670f2db481fddae74ad3
Akshay Bharadva <akshaybharadva19@gmail.com>
2023-07-15 20:43:37 +0530

    fix: error solved

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,8 +1,19 @@
+import Container from "@/components/container";
+import Hero from "@/components/hero";
 import Layout from "@/components/layout";
+import Me from "@/components/me";
+import Technology from "@/components/technology";
+import Tools from "@/components/tools";
 
 export default function Home() {
   return (
-    <Layout title="About">
+    <Layout>
+      <Container>
+        <Me />
+        <Hero />
+        <Technology />
+        <Tools />
+      </Container>
     </Layout>
   );
 }
